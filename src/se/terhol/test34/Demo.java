5c2ded06d23bbec0367b6cbbc8856f1e5af07e5b
Tereza Holm <holm.tereza@gmail.com>
2019-01-16 21:14:08 +0100

    Project finished.

--- src/se/terhol/test34/Demo.java
+++ src/se/terhol/test34/Demo.java
@@ -3,50 +3,48 @@ package se.terhol.test34;
 import java.io.IOException;
 
 /**
- * 
- * @author   Ales  Zlamal
- * @version  2010 11 13
+ * @author Ales  Zlamal
+ * @version 2010 11 13
  */
 public class Demo {
     public static void main(String[] args) throws IOException {
-        House h = null;
-        
+        House h;
+
         System.out.print("Reading data from directConnections.txt...");
         h = new HouseImpl("directConnections.txt");
         System.out.println("OK");
-        
-        
+
         Person p1 = new Person("Louis Charles August");
         Person p2 = new Person("Charlotte");
         Person p3 = new Person("Viviane");
         Person p4 = new Person("Raoul");
         Person p5 = new Person("Alasdair");
         Person p6 = new Person("Raquelle");
-        
+
         Person p7 = new Person("Vasek");
-        
+
         h.placePerson(p1, new Room("grand salon"));
         h.placePerson(p2, new Room("entrance hall"));
         h.placePerson(p3, new Room("grand salon"));
         h.placePerson(p4, new Room("entrance hall"));
         h.placePerson(p5, new Room("workroom"));
         h.placePerson(p6, new Room("connecting gallery"));
-        
-        try{
+
+        try {
             System.out.print("enterNextDoor() -- unknown person: ");
             h.enterNextDoor(p7, new Room("ballroom"));
             System.out.println("ERROR");
-        }catch(IllegalArgumentException iae){
+        } catch (IllegalArgumentException iae) {
             System.out.println("OK");
         }
-        try{
+        try {
             System.out.print("enterNextDoor() -- unknown room: ");
-            h.enterNextDoor(p3, new Room("glasshouse")); 
+            h.enterNextDoor(p3, new Room("glasshouse"));
             System.out.println("ERROR");
-        }catch(IllegalArgumentException iae){
+        } catch (IllegalArgumentException iae) {
             System.out.println("OK");
         }
-        try{
+        try {
             System.out.print("placePerson() -- replacing person: ");
             h.placePerson(p3, new Room("workroom"));
             if (h.whereIs(p3).equals(new Room("workroom"))) {
@@ -54,34 +52,34 @@ public class Demo {
             } else {
                 System.out.println("ERROR");
             }
-        }catch(IllegalArgumentException iae){
+        } catch (IllegalArgumentException iae) {
             System.out.println("ERROR");
         }
-        
-        try{
+
+        try {
             System.out.print(p3);
             h.enterNextDoor(p3, new Room("ballroom"));
             System.out.println(" entered \"ballroom\": ERROR");
-        }catch(IllegalArgumentException due){
+        } catch (IllegalArgumentException due) {
             System.out.println(" doesn't entered \"ballroom\": OK");
         }
-        
-        try{
+
+        try {
             System.out.print("Moving various persons to their neighbouring rooms: ");
-            h.enterNextDoor(p6, new Room("ballroom")); 
-            h.enterNextDoor(p1, new Room("connecting gallery")); 
-            h.enterNextDoor(p2, new Room("connecting gallery")); 
-            h.enterNextDoor(p3, new Room("salon bordeaux")); 
-            h.enterNextDoor(p1, new Room("ballroom")); 
-            h.enterNextDoor(p2, new Room("ballroom")); 
+            h.enterNextDoor(p6, new Room("ballroom"));
+            h.enterNextDoor(p1, new Room("connecting gallery"));
+            h.enterNextDoor(p2, new Room("connecting gallery"));
+            h.enterNextDoor(p3, new Room("salon bordeaux"));
+            h.enterNextDoor(p1, new Room("ballroom"));
+            h.enterNextDoor(p2, new Room("ballroom"));
             System.out.println("OK");
-        }catch(IllegalArgumentException due){
+        } catch (IllegalArgumentException due) {
             System.out.println("ERROR: " + due);
         }
 
-        System.out.print(p3 +" is in \"" + h.whereIs(p3) + "\". ");
+        System.out.print(p3 + " is in \"" + h.whereIs(p3) + "\". ");
         System.out.println(p6 + " is in \"" + h.whereIs(p6) + "\".");
-        
+
         System.out.println();
         System.out.println("savePlan() -- line order doesn't matter: ");
         h.savePlan(System.out);
---
+++ src/se/terhol/test34/HouseImpl.java
@@ -0,0 +1,92 @@
+package se.terhol.test34;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * @author Tereza Holm
+ */
+public class HouseImpl implements House {
+    Map<Room, Set<Room>> myHouse = new HashMap<>();
+    Map<Person, Room> currentHouse = new HashMap<>();
+
+    public HouseImpl(String input) throws IOException {
+        InputStream is = Files.newInputStream(Paths.get(input));
+        this.loadPlan(is);
+    }
+
+    @Override
+    public void placePerson(Person person, Room room) {
+        if (person == null) {
+            throw new IllegalArgumentException();
+        }
+        currentHouse.put(person, room);
+    }
+
+    @Override
+    public Room whereIs(Person person) {
+        return currentHouse.get(person);
+    }
+
+    @Override
+    public void enterNextDoor(Person person, Room room) {
+        if (!currentHouse.containsKey(person) || !this.isNext(currentHouse.get(person), room)) {
+            throw new IllegalArgumentException();
+        }
+        this.placePerson(person, room);
+    }
+
+    @Override
+    public void savePlan(OutputStream os) {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os))) {
+            for (Room room : myHouse.keySet()) {
+                for (Room roomNext : myHouse.get(room)) {
+                    writer.write(room.toString() + ":" + roomNext.toString());
+                    writer.newLine();
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("IO error");
+        }
+    }
+
+    @Override
+    public void loadPlan(InputStream is) {
+        Scanner reader = new Scanner(is);
+
+        String line;
+
+        while (reader.hasNextLine()) {
+            line = reader.nextLine();
+            int spaceInt = line.indexOf(":");
+            Room room1 = new Room(line.substring(0, spaceInt));
+            Room room2 = new Room(line.substring(spaceInt + 1));
+
+            if (!myHouse.containsKey(room1)) {
+                myHouse.put(room1, new HashSet<>());
+            }
+            myHouse.get(room1).add(room2);
+            if (!myHouse.containsKey(room2)) {
+                myHouse.put(room2, new HashSet<>());
+            }
+            myHouse.get(room2).add(room1);
+        }
+    }
+
+    private boolean isNext(Room room1, Room room2) {
+        if (!myHouse.containsKey(room1) || !myHouse.containsKey(room2)) {
+            throw new IllegalArgumentException();
+        }
+        return myHouse.get(room1).contains(room2);
+    }
+}
